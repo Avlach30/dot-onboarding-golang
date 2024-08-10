c9fa6b188fca578d3f7369f62f5a114ae962c13b
Ubaidillah Hakim Fadly <ubaidillahhf@gmail.com>
2024-08-10 21:57:56 +0700

    chore

--- app/dto/user/user_register_req.go
+++ app/dto/user/user_register_req.go
@@ -3,6 +3,6 @@ package userdto
 type RegisterRequest struct {
 	Fullname    string `json:"fullname" validate:"required"`
 	Email       string `json:"email" validate:"required"`
-	PhoneNumber string `json:"phone_number" validate:"required"`
+	PhoneNumber string `json:"phone_number" validate:"required,e164"`
 	Type        string `json:"type"`
 }
