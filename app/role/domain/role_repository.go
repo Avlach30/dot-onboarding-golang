979916d8dd49b0510ff815b95c63e039c70322a1
Daniel Prastha <danielprastha4@gmail.com>
2024-12-17 16:44:50 +0700

    fix: role use gin context

--- app/role/domain/role_repository.go
+++ app/role/domain/role_repository.go
@@ -1,19 +1,18 @@
 package domain
 
 import (
-	"context"
-
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
 type RoleRepository interface {
-	Create(context *context.Context, payload *RoleEntity) error
-	FindById(context *context.Context, id uuid.UUID, trashed bool) (*RoleEntity, error)
-	FindByKey(context *context.Context, key string, trashed bool) (*RoleEntity, error)
-	FindByNameAndKey(context *context.Context, name string, key string) (*RoleEntity, error)
-	Update(context *context.Context, id uuid.UUID, payload *RoleEntity) error
-	Delete(context *context.Context, id uuid.UUID)
-	ForceDelete(context *context.Context, id uuid.UUID)
-	IsKeyExist(context *context.Context, key string) bool
-	IsKeyExistExceptRoleId(context *context.Context, key string, id uuid.UUID) bool
+	Create(ctx *gin.Context, payload *RoleEntity) error
+	FindById(ctx *gin.Context, id uuid.UUID, trashed bool) (*RoleEntity, error)
+	FindByKey(ctx *gin.Context, key string, trashed bool) (*RoleEntity, error)
+	FindByNameAndKey(ctx *gin.Context, name string, key string) (*RoleEntity, error)
+	Update(ctx *gin.Context, id uuid.UUID, payload *RoleEntity) error
+	Delete(ctx *gin.Context, id uuid.UUID)
+	ForceDelete(ctx *gin.Context, id uuid.UUID)
+	IsKeyExist(ctx *gin.Context, key string) bool
+	IsKeyExistExceptRoleId(ctx *gin.Context, key string, id uuid.UUID) bool
 }
