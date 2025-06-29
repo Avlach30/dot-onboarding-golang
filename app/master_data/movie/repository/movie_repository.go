d9a2dc6168b53622a1c88a445c411122fafdf6cc
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2025-06-29 20:13:00 +0700

    feat: initialize query logic for module movie

---
+++ app/master_data/movie/repository/movie_repository.go
@@ -0,0 +1,139 @@
+package repository
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/app/master_data/movie/domain"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/entities"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/interface/http/exception"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/pkg/utils"
+	"gorm.io/gorm"
+)
+
+type MovieRepository struct {
+	model *gorm.DB
+}
+
+func NewMovieRepository(db *gorm.DB) domain.MovieRepository {
+	return &MovieRepository{
+		model: db.Model(&entities.MovieEntity{}),
+	}
+}
+
+func (movie *MovieRepository) Pagination(httpContext *gin.Context) ([]entities.MovieEntity, int) {
+	query := movie.model.WithContext(httpContext)
+	var movies []entities.MovieEntity
+	var total int64
+
+	// Query filter
+	query = movie.queryFilter(query, httpContext)
+	// Query sort
+	query = movie.querySort(query, httpContext)
+
+	
+	err := query.Count(&total).Error
+	if err != nil {
+		log.Println("Error counting movies: ", err)
+		panic(*exception.ServerErrorException(err))
+	}
+
+	err = query.Session(&gorm.Session{}).
+		Scopes(utils.Paginate(httpContext)).
+		Find(&movies).Error
+	if err != nil {
+		log.Println("Error fetching movies: ", err)
+		panic(*exception.ServerErrorException(err))
+	}
+
+	return movies, int(total)
+}
+
+func (movie *MovieRepository) queryFilter(query *gorm.DB, httpContext *gin.Context) *gorm.DB {
+	if search := httpContext.Query("search"); search != "" {
+		query = query.Where("name ILIKE ?", search+"%")
+	}
+
+	return query
+}
+
+func (movie *MovieRepository) querySort(query *gorm.DB, httpContext *gin.Context) *gorm.DB {
+	sortableColumns := []string{"name", "chair_capacity", "duration_in_minutes", "updated_at"}
+
+	if sort := httpContext.Query("sort_by"); sort != "" {
+		// Check if the sort column is valid
+		if !utils.Contains(sortableColumns, sort) {
+			panic(*exception.BussinessException("Invalid sort column"))
+		}
+
+		// Handle order query
+		if order := httpContext.Query("order"); order != "" {
+			if order != "asc" && order != "desc" {
+				panic(*exception.BussinessException("Invalid order value"))
+			}
+
+			query = query.Order(sort + " " + order)
+		}
+	} else {
+		query = query.Order("updated_at desc")
+	}
+
+	return query
+}
+
+func (movie *MovieRepository) Create(httpContext *gin.Context, payload *entities.MovieEntity) {
+	movie.model = movie.model.WithContext(httpContext)
+
+	err := movie.model.Create(payload).Error
+	if err != nil {
+		log.Println("Error creating movie:", err)
+		panic(*exception.ServerErrorException(err))
+	}
+}
+
+func (movie *MovieRepository) FindOneById(httpContext *gin.Context, id uuid.UUID, trashed bool) *entities.MovieEntity {
+	movie.model = movie.model.WithContext(httpContext)
+	movieEntity := &entities.MovieEntity{}
+	if trashed {
+		movie.model = movie.model.Unscoped()
+	}
+
+	err := movie.model.
+		First(&movieEntity, id).
+		Error
+	if err == gorm.ErrRecordNotFound {
+		panic(*exception.NotFoundException("Movie not found"))
+	} else if err != nil {
+		log.Println("Error movie find by id:", err)
+		panic(*exception.ServerErrorException(err))
+	}
+
+	return movieEntity
+}
+
+func (movie *MovieRepository) Update(httpContext *gin.Context, id uuid.UUID, payload *entities.MovieEntity) {
+	movie.model = movie.model.WithContext(httpContext)
+
+	err := movie.model.
+		Where("id = ?", id).
+		Updates(payload).
+		Error
+	if err != nil {
+		log.Println("Error updating movie:", err)
+		panic(*exception.ServerErrorException(err))
+	}
+}
+
+func (movie *MovieRepository) Delete(httpContext *gin.Context, id uuid.UUID) {
+	movie.model = movie.model.WithContext(httpContext)
+
+	err := movie.model.
+		Delete(&entities.MovieEntity{}, id).
+		Error
+	if err != nil {
+		log.Println("Error deleting movie:", err)
+		panic(*exception.ServerErrorException(err))
+	}
+}
+
