ee6431c5a48d5c429ba090dbcba7caec8d4a5aa1
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2025-06-29 22:03:55 +0700

    feat: initialize business logic for module movie schedule

---
+++ app/master_data/movie_schedule/usecase/movie_schedule_usecase.go
@@ -0,0 +1,38 @@
+package usecase
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/app/master_data/movie_schedule/domain"
+	"gitlab.dot.co.id/playground/boilerplates/golang-service/entities"
+)
+
+type MovieScheduleUsecase struct {
+	movieScheduleRepository domain.MovieScheduleRepository
+}
+
+func NewMovieScheduleUsecase(movieScheduleRepository domain.MovieScheduleRepository) *MovieScheduleUsecase {
+	return &MovieScheduleUsecase{
+		movieScheduleRepository: movieScheduleRepository,
+	}
+}
+
+func (movieScheduleUsecase *MovieScheduleUsecase) Pagination(httpContext *gin.Context) ([]entities.MovieScheduleEntity, int) {
+	return movieScheduleUsecase.movieScheduleRepository.Pagination(httpContext)
+}
+
+func (movieScheduleUsecase *MovieScheduleUsecase) Create(httpContext *gin.Context, payload *entities.MovieScheduleEntity) {
+	movieScheduleUsecase.movieScheduleRepository.Create(httpContext, payload)
+}
+
+func (movieScheduleUsecase *MovieScheduleUsecase) FindOneById(httpContext *gin.Context, id uuid.UUID, trashed bool) *entities.MovieScheduleEntity {
+	return movieScheduleUsecase.movieScheduleRepository.FindOneById(httpContext, id, trashed)
+}
+
+func (movieScheduleUsecase *MovieScheduleUsecase) Update(httpContext *gin.Context, id uuid.UUID, payload *entities.MovieScheduleEntity) {
+	movieScheduleUsecase.movieScheduleRepository.Update(httpContext, id, payload)
+}
+
+func (movieScheduleUsecase *MovieScheduleUsecase) Delete(httpContext *gin.Context, id uuid.UUID) {
+	movieScheduleUsecase.movieScheduleRepository.Delete(httpContext, id)
+}
